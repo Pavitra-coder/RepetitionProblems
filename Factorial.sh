04e5f2449d1736b0c4f7c900df127583de5993ff
Pavitra-coder <pavitrabhalke456@gmail.com>
2020-04-26 22:54:52 +0530

    Factorial of a given number

---
+++ Factorial.sh
@@ -0,0 +1,12 @@
+5)Write a program that computes a factorial of a number taken as input.
+
+#!/bin/bash -x
+echo "Enter a number:"
+read n
+f=1
+for((i = 1 ; i <= n ; i++))
+do
+f=`expr $f \* $i`
+done
+echo "The factorial of $n is $f"
+
